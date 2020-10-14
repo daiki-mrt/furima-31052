574e1094d71ee2c309c2fdccc4c78c3a4c54d198
daiki-mrt <70130536+daiki-mrt@users.noreply.github.com>
2020-10-14 13:21:13 +0900

    ロールバック、userモデル削除

--- db/schema.rb
+++ db/schema.rb
@@ -12,4 +12,20 @@
 
 ActiveRecord::Schema.define(version: 0) do
 
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.string "nickname", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "first_name_kanji", null: false
+    t.string "family_name_kanji", null: false
+    t.string "first_name_kana", null: false
+    t.string "family_name_kana", null: false
+    t.date "birth", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
 end
