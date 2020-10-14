a838f4d5d89ffd4f6747f972731a9dbece2b6dde
daiki-mrt <70130536+daiki-mrt@users.noreply.github.com>
2020-10-14 17:04:22 +0900

    新規登録、登録失敗エラー表示の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :family_name_kanji, :first_name_kanji, :family_name_kana, :first_name_kana, :birth])    
+  end
 end
