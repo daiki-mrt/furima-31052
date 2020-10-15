1d75a760d336a81ca71c06e835d94bea06cf426d
daiki-mrt <70130536+daiki-mrt@users.noreply.github.com>
2020-10-15 17:49:10 +0900

    販売手数料、利益の表示

---
+++ app/javascript/price.js
@@ -0,0 +1,23 @@
+function price () {
+  const priceForm = document.getElementById("item-price")
+  priceForm.addEventListener("keyup", () => {
+    const price = priceForm.value
+    const marginForm = document.getElementById("add-tax-price")
+    const profitForm = document.getElementById("profit")
+    const margin = Math.floor(price * 0.1)
+    const profit = price - margin
+    if (price < 10) {
+      marginForm.innerHTML = 0
+      profitForm.innerHTML = 0      
+    } else if (price > 10) {
+      marginForm.innerHTML = margin.toLocaleString()
+      profitForm.innerHTML = profit.toLocaleString()
+    }
+  })
+}
+window.addEventListener("load", price)
+
+function hoge () {
+  console.log("hoge")
+}
+window.addEventListener("load", hoge)
