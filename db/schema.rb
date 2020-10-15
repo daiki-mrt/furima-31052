98e584aa22194033725e8fb8f17c8fe073e5bd34
daiki-mrt <70130536+daiki-mrt@users.noreply.github.com>
2020-10-15 12:34:31 +0900

    商品情報にActiveHash導入

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_15_021555) do
+ActiveRecord::Schema.define(version: 2020_10_15_033057) do
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2020_10_15_021555) do
     t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "status_id"
     t.index ["user_id"], name: "index_items_on_user_id"
   end
 
