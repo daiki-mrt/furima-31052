4947a7c2668b2c126084a3c00703616d6ae0ab02
daiki-mrt <70130536+daiki-mrt@users.noreply.github.com>
2020-10-19 14:56:25 +0900

    売却済み商品の削除制限

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -35,7 +35,7 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    if user_signed_in? && @item.user.id == current_user.id
+    if (user_signed_in? && @item.user.id == current_user.id) && @item.order.nil?
       @item.destroy
       redirect_to root_path
     else
