6d4f59cf116813d204b7babd90d3df4d4712ae73
daiki-mrt <70130536+daiki-mrt@users.noreply.github.com>
2020-10-19 17:44:54 +0900

    ordersコントローラー修正　create条件分岐、購入ページ遷移制限

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,13 +1,16 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_item
+  before_action :move_to_index
+
   def index
-    @item  = Item.find(params[:item_id])
     @order_address = OrderAddress.new
   end
 
   def create
-
     @order_address = OrderAddress.new(order_address_params)
-    if @order_address.save
+    if @order_address.valid?
+      @order_address.save
       redirect_to root_path
     else
       render :index
@@ -17,6 +20,16 @@ class OrdersController < ApplicationController
   private
   def order_address_params
     params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :street_num, :building, :phone_num).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def move_to_index
+    if (@item.user.id == current_user.id) || @item.order
+      redirect_to root_path
+    end
   end
 end
   
