03022dd72a20eb18a538d3f309f59ce6e4349f63
daiki-mrt <70130536+daiki-mrt@users.noreply.github.com>
2020-10-21 18:29:48 +0900

    deploy.rb訂正

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ lock "~> 3.14.1"
 set :application, "furima-31052"
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url, "git@example.com:daiki-mrt/furima-31052.git"
+set :repo_url, "git@github.com:daiki-mrt/furima-31052.git"
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
